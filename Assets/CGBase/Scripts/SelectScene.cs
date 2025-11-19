d9198ca280cb4f6c5ba6f12fbb673e9746cfb334
Jakald <jakald.magno@hotmail.com>
2025-11-18 20:41:55 -0600

    Modified Select Scene
    Now can change betwen Center Operations and Play Scene.

--- Assets/CGBase/Scripts/SelectScene.cs
+++ Assets/CGBase/Scripts/SelectScene.cs
@@ -3,35 +3,39 @@ using UnityEngine;
 
 public class SelectScene : MonoBehaviour
 {
-public string SceneToLoad;
+    private Camera camara;
 
-private Camera camara;
-
-void Start()
-{
-        // Se guarda posición de la cámara
-    camara = GetComponent<Camera>();
-}
-
-void Update()
-{
-        //Para activar la selección, usamos el botón del centro del mouse.
-    if (Input.GetMouseButtonDown(2))
+    void Start()
     {
-            //Se hace un rayo para detectar el modelo
-        Ray rayo = camara.ScreenPointToRay(Input.mousePosition);
+        // Se guarda posición de la cámara
+        camara = GetComponent<Camera>();
+    }
 
-        RaycastHit hit;
-
-        if (Physics.Raycast(rayo, out hit))
+    void Update()
+    {
+        //Para activar la selección, usamos el botón del centro del mouse.
+        if (Input.GetMouseButtonDown(2))
         {
-                //Si se detecta, carga la escena
-            if (hit.collider.CompareTag("opCenter"))
+            //Se hace un rayo para detectar el modelo
+            Ray rayo = camara.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(rayo, out hit))
             {
-                Debug.Log("Cargando Escena: " + SceneToLoad);
-                SceneManager.LoadScene(SceneToLoad);
+                //Detecta si colisiona con un objeto
+                TargetScene target = hit.collider.GetComponent<TargetScene>();
+
+                //Si no es nulo, intenta cargar la escena.
+                if (target != null) {
+                    string invoque = target.sceneName;
+
+                    if (!string.IsNullOrEmpty(invoque))
+                    {
+                        Debug.Log("Cargando Escena" + invoque);
+                        SceneManager.LoadScene(invoque);
+                    }
+                }
             }
         }
     }
-}
 }
---
+++ Assets/CGBase/Scripts/TargetScene.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class TargetScene : MonoBehaviour
+{
+    public string sceneName;
+}
